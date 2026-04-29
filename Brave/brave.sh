b54fe7769ff1711d69ff5a2af54acf0d0a76807d
gnukeith <132023371+gnukeith@users.noreply.github.com>
2026-04-29 13:49:12 -0400

    I want icecream

--- Brave/brave.sh
+++ Brave/brave.sh
@@ -7,8 +7,22 @@ printf '%s\n' '{
   "BraveWalletDisabled": true,
   "BraveVPNDisabled": 1,
   "BraveAIChatEnabled": false,
-  "BraveTalkDisabled": false,
+  "BraveTalkDisabled": true,
   "TorDisabled": true,
+
+  "PasswordManagerEnabled": false,
+  "AutofillCreditCardEnabled": false,
+  "AutofillAddressEnabled": false,
+
+  "EnableMediaRouter": false,
+
+  "BraveP3AEnabled": false,
+  "BraveStatsPingEnabled": false,
+  "MetricsReportingEnabled": false,
+  "BraveWebDiscoveryEnabled": false,
+
   "DnsOverHttpsMode": "secure",
-  "DnsOverHttpsTemplates": "https://base.dns.mullvad.net/dns-query"
-}' | sudo tee /etc/brave/policies/managed/brave_policies.json > /dev/null
+  "DnsOverHttpsTemplates": "https://base.dns.mullvad.net/dns-query",
+
+  "DefaultJavaScriptJitSetting": 2
+}' | sudo tee /etc/brave/policies/managed/brave-policies.json > /dev/null
